d85722003ce59ffad18844f53cd551eb34792915
Fabio <fabio_rdg@msn.com>
2022-05-16 21:16:54 -0300

    arquivos iniciais terraform do projeto

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "project_id" {
+#  type    = "string"
+  default = "neon-chimera-348917"
+}
+
+variable "regiao" {
+#  type = "string"
+  default = "us-east1"
+}
+
+variable "nome" {
+#  type = "string"
+  default = "vm-webserver"
+}
+
+variable "tipo_maquina" {
+#  type = "string"
+  default = "f1-micro"
+}
+
+variable "zona" {
+  default = "us-east1-b"
+}
+
+variable "imagem" {
+#  type = "string"
+  default = "debian-cloud/debian-9"
+}
+
